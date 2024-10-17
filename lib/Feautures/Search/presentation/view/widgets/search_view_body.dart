12d944454f060cdc1b185a55775a5895bcb6b2b2
AmerShammout <amershammout2004@gmail.com>
2024-10-17 21:45:57 +0300

    search view ui done

--- lib/Feautures/Search/presentation/view/widgets/search_view_body.dart
+++ lib/Feautures/Search/presentation/view/widgets/search_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:bookly_app/Feautures/Search/presentation/view/widgets/custom_search_text_field.dart';
+import 'package:bookly_app/Feautures/Search/presentation/view/widgets/search_results_list_view.dart';
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -7,14 +9,42 @@ class SearchViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const [
-        SizedBox(
-          height: 30,
+    return Column(
+      children: [
+        const SizedBox(
+          height: 40,
         ),
-        Padding(
+        const Padding(
           padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
           child: CustomSearchTextField(),
+        ),
+        Expanded(
+          child: CustomScrollView(
+            slivers: [
+              const SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 20,
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: kHorizontalPadding,
+                  ),
+                  child: Text(
+                    'Search Result',
+                    style: AppStyles.styleSemiBold18(context),
+                  ),
+                ),
+              ),
+              const SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 16,
+                ),
+              ),
+              const SearchResultsListView(),
+            ],
+          ),
         ),
       ],
     );
