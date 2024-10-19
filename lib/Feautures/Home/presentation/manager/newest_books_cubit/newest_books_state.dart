3bdb0fef77d5ba8247caf2468ae10dca99df1bd2
AmerShammout <amershammout2004@gmail.com>
2024-10-19 12:30:51 +0300

    newest books cubit done

--- lib/Feautures/Home/presentation/manager/newest_books_cubit/newest_books_state.dart
+++ lib/Feautures/Home/presentation/manager/newest_books_cubit/newest_books_state.dart
@@ -8,3 +8,15 @@ sealed class NewestBooksState extends Equatable {
 }
 
 final class NewestBooksInitial extends NewestBooksState {}
+final class NewestBooksLoading extends NewestBooksState {}
+final class NewestBooksFailure extends NewestBooksState {
+  final String errMessage;
+
+  const NewestBooksFailure({required this.errMessage});
+}
+final class NewestBooksSuccess extends NewestBooksState {
+  final List<BookModel>books;
+
+  const NewestBooksSuccess({required this.books});
+}
+
